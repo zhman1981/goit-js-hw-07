2f086aae5d692373166d2b9593d1eb5f8b2540c2
zhman1981 <zhman@ukr.net>
2022-08-14 20:55:39 +0300

    Ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,42 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
 // Change code below this line
 
 const makeGallary = () => {
     return galleryItems.map(item => {
         return `
         <div class="gallery__item">
-            <!--<a class="gallery__link" href=${item.original}>-->
-                <img
-                class="gallery__image"
-                src=${item.preview}
-                data-source=${item.original}
-                alt=${item.description}
-                />
-            <!--</a>-->
+          <a class="gallery__link" href=${item.original}>
+            <img
+            class="gallery__image"
+            src=${item.preview}
+            data-source=${item.original}
+            alt=${item.description}
+            />
+          </a>
         </div>
         `
     }).join('');
 }
 
 const onPreveiwClick = (evt) => {
+    evt.preventDefault();
     if (evt.target.nodeName !== 'IMG') { return }
     const instance = basicLightbox.create(`
-        <div class="modal">
-            <p>
-                Your first lightbox with just a few lines of code.
-                Yes, it's really that simple.
-            </p>
-        </div>
+        <img src="${evt.target.dataset.source}">
     `)
-
-    instance.show()
-    console.log(evt.target.src);
+    instance.show();
+    if (instance.visible) {
+        addEventListener('keydown', evt => {
+            if (evt.code === 'Escape') {
+                instance.close()
+            }
+        })
+    }
 }
 
 const divGallary = document.querySelector('div.gallery');
 divGallary.insertAdjacentHTML("beforeend", makeGallary());
 divGallary.addEventListener('click', onPreveiwClick);
-
-
-// console.log(makeGallary());
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const makeGallary = () => {
+    return galleryItems.map(item => {
+        return `
+        <a class="gallery__item" href=${item.original}>
+           <img class="gallery__image" src=${item.preview} alt=${item.description}/>
+        </a>
+        `
+    }).join('');
+}
+
+const onPreveiwClick = (evt) => {
+    evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {});
+    lightbox.defaultOptions.captionsData = 'alt';
+    lightbox.defaultOptions.captionDelay = 250;
+}
+
+const divGallary = document.querySelector('div.gallery');
+divGallary.insertAdjacentHTML("beforeend", makeGallary());
+divGallary.addEventListener('click', onPreveiwClick);
